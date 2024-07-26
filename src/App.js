8c56f9d61f7d88be83632ad88e69c3bb328b431b
Steven Webb <u7544998@anu.edu.au>
2024-07-26 21:01:02 +1000

    add several pages

--- src/App.js
+++ src/App.js
@@ -1,3 +1,36 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { Suspense, lazy } from 'react';
+
+// Lazy load each section
+const Home = lazy(() => import('./pages/Home'));
+const Organizer = lazy(() => import('./pages/Organizer'));
+const Event = lazy(() => import('./pages/Event'));
+const Admin = lazy(() => import('./pages/Admin'));
+//const Designer = lazy(() => import('./pages/Designer'));
+const User = lazy(() => import('./pages/User'));
+
+function App() {
+    return (
+        <Router>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/organizer/*" element={<Organizer />} />
+                    <Route path="/event/*" element={<Event />} />
+                    <Route path="/admin/*" element={<Admin />} />
+                    {/*
+                    <Route path="/designer/*" element={<Designer />} />
+                    */ }
+                    <Route path="/user/*" element={<User />} />
+                </Routes>
+            </Suspense>
+        </Router>
+    );
+}
+
+export default App;
+
+/*
 import {
     BrowserRouter as Router, Route, Routes, Navigate
 } from 'react-router-dom';
@@ -17,3 +50,4 @@ function App() {
 }
 
 export default App;
+*/
