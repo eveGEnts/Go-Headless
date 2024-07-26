262b0513c05b8820a29e39c65d97e822a8cfeb28
Steven Webb <u7544998@anu.edu.au>
2024-07-26 21:43:08 +1000

    quicklinks

--- src/components/home/links.js
+++ src/components/home/links.js
@@ -1,34 +1,43 @@
 import React from 'react';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
+import './links.scss';
+
 const Links = () => {
   return (
-    <div className="card card-primary card-outline mb-3">
-      <div className="card-header">
-        <h3 className="card-title">
-          <i className="fa-solid fa-compass"></i>&nbsp;Quick Links
-        </h3>
-      </div>
-      <div className="card-body">
+    <div className="container">
         <div className="row menu-row">
-          <div className="col-md-4 menu-col">
-            <div className="menu-option p-3" onClick={() => {}}>
-              <div><i className="fa fa-calendar-week fa-super"></i></div>
-              <div>Event List</div>
+
+            <div className="col-md-4 menu-col">
+                <div className="menu-option p-3" onClick={() => {}}>
+                    <div><i className="fa fa-tasks fa-super"></i></div>
+                    <div className='fw-bold'>Organizer Module</div>
+                </div>
             </div>
-          </div>
-          <div className="col-md-4 menu-col">
-            <div className="menu-option p-3" onClick={() => {}}>
-              <div><i className="fa fa-calendar-check fa-super"></i></div>
-              <div>Event Calendar</div>
+
+            <div className="col-md-4 menu-col">
+                <div className="menu-option p-3" onClick={() => {}}>
+                    <div><i className="fa fa-calendar-check fa-super"></i></div>
+                    <div className='fw-bold'>Event Pages</div>
+                </div>
             </div>
-          </div>
-          <div className="col-md-4 menu-col">
-            <div className="menu-option p-3" onClick={() => {}}>
-              <div><i className="fa fa-calendar-plus fa-super"></i></div>
-              <div>Add Event</div>
+
+            <div className="col-md-4 menu-col">
+                <div className="menu-option p-3" onClick={() => {}}>
+                    <div><i className="fa fa-shield-halved fa-super"></i></div>
+                    <div className='fw-bold'>System Administration</div>
+                </div>
             </div>
-          </div>
+
+        </div>
+    </div>
+  );
+};
+
+export default Links;
+
+/*
+            
           <div className="col-md-4 menu-col">
             <div className="menu-option p-3" onClick={() => {}}>
               <div><i className="fa-solid fa-cloud-arrow-up fa-super"></i></div>
@@ -65,10 +74,4 @@ const Links = () => {
               <div>User Profile</div>
             </div>
           </div>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default Links;
+          */
