50d7a99e6e98a12a6e94b2a0760d2fa82cd94959
your_github_username <your_github_email>
2024-07-31 16:40:03 +1000

    better project structure

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import React, { Suspense, lazy } from 'react';
 
 // Lazy load each section
-const Home = lazy(() => import('./pages/Home'));
+const Home = lazy(() => import('./pages/Home/Home'));
 const Organizer = lazy(() => import('./pages/Organizer'));
 const Event = lazy(() => import('./pages/Event'));
 const Admin = lazy(() => import('./pages/Admin'));
@@ -29,25 +29,3 @@ function App() {
 }
 
 export default App;
-
-/*
-import {
-    BrowserRouter as Router, Route, Routes, Navigate
-} from 'react-router-dom';
-import Home from './pages/Home';
-
-import './App.css';
-
-function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Navigate replace to="/home.html" />} />
-                <Route path="/home.html" element={<Home />} />
-            </Routes>
-        </Router>
-    );
-}
-
-export default App;
-*/
--- src/index.js
+++ src/index.js
@@ -1,25 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-//import './index.css';
 import App from './App';
-//import reportWebVitals from './reportWebVitals';
-
-
-//import About from './components/About';
-//import Contact from './components/Contact';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
-import './custom.scss';
+import './styles/style.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-//reportWebVitals();
