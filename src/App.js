c647fd3b3b41053c7624069ffb95a0d8c1692857
your_github_username <your_github_email>
2024-07-31 16:52:05 +1000

    navbar

--- src/App.js
+++ src/App.js
@@ -3,11 +3,6 @@ import React, { Suspense, lazy } from 'react';
 
 // Lazy load each section
 const Home = lazy(() => import('./pages/Home/Home'));
-const Organizer = lazy(() => import('./pages/Organizer'));
-const Event = lazy(() => import('./pages/Event'));
-const Admin = lazy(() => import('./pages/Admin'));
-//const Designer = lazy(() => import('./pages/Designer'));
-const User = lazy(() => import('./pages/User'));
 
 function App() {
     return (
@@ -15,13 +10,6 @@ function App() {
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
                     <Route path="/" element={<Home />} />
-                    <Route path="/organizer/*" element={<Organizer />} />
-                    <Route path="/event/*" element={<Event />} />
-                    <Route path="/admin/*" element={<Admin />} />
-                    {/*
-                    <Route path="/designer/*" element={<Designer />} />
-                    */ }
-                    <Route path="/user/*" element={<User />} />
                 </Routes>
             </Suspense>
         </Router>
