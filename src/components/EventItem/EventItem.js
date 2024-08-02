133ea2a12a0d0a6fef6669cb0e49599469c22876
Steven Webb <u7544998@anu.edu.au>
2024-08-02 13:55:38 +1000

    height

--- src/components/EventItem/EventItem.js
+++ src/components/EventItem/EventItem.js
@@ -7,7 +7,7 @@ import './EventItem.scss';
 
 function EventItem({ banner, name, location, time, enabled, organizer, ticketed }) {
     const renderTicketInfo = () => {
-        switch(ticketed) {
+        switch (ticketed) {
             case true:
                 return <div className="small"><i className="fa-solid fa-ticket"></i>&nbsp;Paid ticket</div>;
             case false:
@@ -19,30 +19,32 @@ function EventItem({ banner, name, location, time, enabled, organizer, ticketed 
     };
 
     let initiallyFavorite = false;
-    if(!enabled)    initiallyFavorite = null;
+    if (!enabled) initiallyFavorite = null;
 
     return (
         <div className={`event-item ${enabled ? '' : 'disabled'}`} role="button" tabIndex="0" aria-disabled={!enabled}>
+            <div className="content-container"> {/* This container holds all content except the footer */}
+                <div className="banner-container">
+                    <img src={banner} alt={name} style={{ width: '100%', height: 'auto' }} />
+                    <FavoriteIcon initiallyFavorite={initiallyFavorite} />
+                </div>
 
-            {/* Event Banner */}
-            <div className="banner-container">
-                <img src={banner} alt={name} style={{ width: '100%', height: 'auto' }} />
-                <FavoriteIcon initiallyFavorite={initiallyFavorite} /> {/* Use the FavoriteIcon component */}
+                <div className='p-3'>
+                    <h5 className="fw-bold text-secondary" id="eventNameLabel">{name}</h5>
+                    <div className="small mb-1">
+                        <i className="fa-regular fa-clock" aria-label="Event time"></i>&nbsp;{time}
+                    </div>
+                    <div className="small mb-1">
+                        <i className="fa-solid fa-location-dot" aria-label="Event location"></i>&nbsp;{location}
+                    </div>
+                    <div className="small mb-1">
+                        <i className="fa-solid fa-users" aria-label="Event organizer"></i>&nbsp;{organizer}
+                    </div>
+                    {renderTicketInfo()}
+                </div>
             </div>
-
-            {/* Event Information */}
-            <div className='p-3'>
-                <h5 className="fw-bold text-secondary" id="eventNameLabel">{name}</h5>
-                <div className="small mb-1">
-                    <i className="fa-regular fa-clock" aria-label="Event time"></i>&nbsp;{time}
-                </div>
-                <div className="small mb-1">
-                    <i className="fa-solid fa-location-dot" aria-label="Event location"></i>&nbsp;{location}
-                </div>
-                <div className="small mb-1">
-                    <i className="fa-solid fa-users" aria-label="Event organizer"></i>&nbsp;{organizer}
-                </div>
-                {renderTicketInfo()}
+            <div className="footer-content"> {/* This is the footer */}
+                TODO: PLACE HERE
             </div>
         </div>
     );
